453d17842f6b6aa5155d3c26d58ade0803cb1c1b
anthonycc <anthonyccardozo@gmail.com>
2021-07-04 18:53:52 -0400

    rebuilt currentConditions function

--- js/script.js
+++ js/script.js
@@ -13,63 +13,28 @@ const form = document.getElementById("form");
 
 //////// FUNCTIONS ////////
 
+// show searched city's current conditions
 function currentConditions(city) {
-  fetch(
-    "https:/api.openweathermap.org/data/2.5/weather/?q=" +
-      city +
-      "&appid=" +
-      key
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      console.log(data);
-      lat = data.coord.lat;
-      lon = data.coord.lon;
-      icon = data.weather[0].icon;
-      day = new Date(data.dt * 1000).toLocaleDateString("en-US");
-      $(".card-group").html(" ");
-      fiveDayFc();
+    fetch(apiCity + city + key).then(function (response) {
+        // if request was successful
+        if (response.ok) {
+            response.json().then(function (data) {
+                //get coordinates for forecast
+                console.log(data);
+                lat = data.coord.lat;
+                lon = data.coord.lon;
+                icon = data.weather[0].icon;
+                // run 5day forecast and current conditions
+                fiveDayFc(city);
+            });
+        } else {
+            // if request unsuccessful
+            savedSearch.shift();
+            // send to LS
+            localStorage.setItem("cities", JSON.stringify(savedSearch));
+            // alert user to failed search
+            alert("Sorry, we couldn't find that one. Try another!");
+            location.reload();
+        }
     });
-}
-
-function fiveDayFc() {
-  fetch(
-    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-      lat + "&lat=" + lon + "&exclude=current,minutely,hourly&units=metric&appid=" + key)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
-
-  $("#currentCity").html(
-    `${city} ${day} <img src='http://openweathermap.org/img/w/${icon}.png'/>`
-  );
-  $("#temp").html(`Temp: ${data.daily[0].temp.day} Celsius`);
-  $("#wind").html(`Wind: ${data.daily[0].wind_speed} KM/hour`);
-  $("#humidity").html(`humidity: ${data.daily[0].humidity} %`);
-  $("#uv").html(`uv: ${data.daily[0].uvi} C`);
-  for (let i = 1; i < 6; i++) {
-    //show weather icon
-    icon = data.daily[i].weather[0].icon;
-    // get normal date format
-    day = new Date(data.daily[i].dt * 1000).toLocaleDateString("en-US", {
-      timeZone: `${data.timeZone}`,
-    });
-    // populate forecast cards
-    $(".forecastCards").append(`
-            <div class = "col-auto mb-3">
-            <div class = "card text-light cardbg no-gutters" style = "width: 14rem;">
-                <div class = "card-body">
-                    <h5 class = "card-title">${day}</h5>
-                    <p class = "card-text font-weight-bold"> <img src = 'http://openweathermap.org/img/w/${icon}.png'/></p>
-                    <p class = "card-text font-weight-bold> Temp: {data.daily[i].temp.day} Celsius </p>
-                    <p class = "card-text font-weight-bold">Wind: ${data.daily[i].wind_speed} KM/hour </p>
-                    <p class = "card-text font-weight-bold">Humidity: ${data.daily[i].humidity} % </p> 
-                </div>
-            </div>`);
-  }
 }
